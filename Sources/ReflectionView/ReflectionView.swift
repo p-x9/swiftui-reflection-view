80d9997dc4c23f13cc467e99736aa224f03b3f06
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-02-07 18:20:43 +0900

    make public

--- Sources/ReflectionView/ReflectionView.swift
+++ Sources/ReflectionView/ReflectionView.swift
@@ -1,16 +1,16 @@
 import SwiftUI
 import SwiftUIColor
 
-struct ReflectionView: View {
+public struct ReflectionView: View {
     let reflection: Reflection
 
     @Environment(\.font) var font
 
-    init(_ value: Any) {
+    public init(_ value: Any) {
         self.reflection = .init(value)
     }
 
-    var body: some View {
+    public var body: some View {
         HStack {
             Text(reflection.description)
                 .font(font ?? .system(size: 14, weight: .bold, design: .monospaced))
