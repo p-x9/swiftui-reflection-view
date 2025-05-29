8857b9f6ab9387879cd60a75921167626ef35426
p-x9 <50244599+p-x9@users.noreply.github.com>
2025-05-29 23:40:11 +0900

    Add test cases for converting Optional, Array, and Dictionary types to syntax sugar

--- Tests/ReflectionViewTests/ReflectionViewTests.swift
+++ Tests/ReflectionViewTests/ReflectionViewTests.swift
@@ -10,4 +10,36 @@ final class ReflectionViewTests: XCTestCase {
         let reflection = Reflection(text)
         print(reflection.structured)
     }
+
+    func testReplacedToOptionalSyntaxSugar_Single() {
+        XCTAssertEqual("Optional<Int>".replacedToOptionalSyntaxSugar, "Int?")
+        XCTAssertEqual("Optional<Optional<Int>>".replacedToOptionalSyntaxSugar, "Int??")
+        XCTAssertEqual("Optional<Array<Int>>".replacedToOptionalSyntaxSugar, "Array<Int>?")
+    }
+
+    func testReplacedToArraySyntaxSugar_Single() {
+        XCTAssertEqual("Array<Int>".replacedToArraySyntaxSugar, "[Int]")
+        XCTAssertEqual("Array<Array<Int>>".replacedToArraySyntaxSugar, "[[Int]]")
+        XCTAssertEqual("Array<Optional<Int>>".replacedToArraySyntaxSugar, "[Optional<Int>]")
+    }
+
+    func testReplacedToDictionarySyntaxSugar_Single() {
+        XCTAssertEqual("Dictionary<String, Int>".replacedToDictionarySyntaxSugar, "[String: Int]")
+        XCTAssertEqual("Dictionary<String, Dictionary<String, Int>>".replacedToDictionarySyntaxSugar, "[String: [String: Int]]")
+        XCTAssertEqual("Dictionary<String, Optional<Int>>".replacedToDictionarySyntaxSugar, "[String: Optional<Int>]")
+    }
+
+    func testReplacedToCommonSyntaxSugar_Combined() {
+        let case1 = "Optional<Array<Dictionary<String, Int>>>"
+        XCTAssertEqual(case1.replacedToCommonSyntaxSugar, "[[String: Int]]?")
+
+        let case2 = "Array<Optional<Dictionary<String, Optional<Int>>>>"
+        XCTAssertEqual(case2.replacedToCommonSyntaxSugar, "[[String: Int?]?]")
+
+        let case3 = "Optional<Dictionary<Optional<String>, Optional<Array<Optional<Int>>>>>"
+        XCTAssertEqual(case3.replacedToCommonSyntaxSugar, "[String?: [Int?]?]?")
+
+        let case4 = "Optional<Array<Dictionary<String, Dictionary<String, Optional<Int>>>>>"
+        XCTAssertEqual(case4.replacedToCommonSyntaxSugar, "[[String: [String: Int?]]]?")
+    }
 }
